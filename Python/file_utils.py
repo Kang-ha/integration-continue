79d6c92bd410ff8ff48fb9a3454b199dc7cbfd21
Kang-ha <dujardin.k@stjolorient.fr>
2025-01-13 16:20:41 +0100

    Correction de fonction

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -2,7 +2,7 @@
 
 def diff(file_first, file_second):
     """Fonction retournant True si deux fichiers sont différents."""
-	with open(file_first as file1_id :
+	with open(file_first as file1_id) :
 		with open(file_second) as file_id2 :
 			return file1_id.read() != file2_id.read()
 	return True
