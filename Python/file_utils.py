6adbf030df4c8cd609ede330b2f1d7f0746be304
Kang-ha <dujardin.k@stjolorient.fr>
2025-01-13 16:08:18 +0100

    Mise à jour de fonction

--- Python/file_utils.py
+++ Python/file_utils.py
@@ -2,8 +2,9 @@
 
 def diff(file_first, file_second):
     """Fonction retournant True si deux fichiers sont différents."""
-    return True
-
+    if file_first == file_second :	
+		return True
+		
 def same(file_first, file_second):
     """Fonction retournant True si deux fichiers sont identiques."""
     return True
